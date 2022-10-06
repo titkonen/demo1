63dbd3ab07bd1b0c0362e6caf4709b3acc241d1e
Toni Itkonen <toni.itkonen@icloud.com>
2022-10-06 20:12:59 +0300

    update

--- demo1/demo1/SecondView.swift
+++ demo1/demo1/SecondView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct SecondView: View {
     var body: some View {
+      VStack {
         Text("Second view updated!")
+      
+      Text("Second view updated!")
+      }
     }
 }
 
