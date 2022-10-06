d2654a2c3c60f73b8763a4e48bf9e0449727d6b0
Toni Itkonen <toni.itkonen@icloud.com>
2022-10-06 19:58:24 +0300

    Second view

---
+++ demo1/demo1/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  demo1
+//
+//  Created by Toni Itkonen on 6.10.2022.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text("Second view!")
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
