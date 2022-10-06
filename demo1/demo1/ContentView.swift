cd1b0c5e50d727f3363c2da68c4ebe715ede84d0
Toni Itkonen <toni.itkonen@icloud.com>
2022-10-06 19:52:54 +0300

    Initial commit

---
+++ demo1/demo1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  demo1
+//
+//  Created by Toni Itkonen on 6.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("view 1")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ demo1/demo1/demo1App.swift
@@ -0,0 +1,17 @@
+//
+//  demo1App.swift
+//  demo1
+//
+//  Created by Toni Itkonen on 6.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct demo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
