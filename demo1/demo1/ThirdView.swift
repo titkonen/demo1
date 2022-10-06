087941d965fb4669e9e1ccb5f559366a442f1709
Toni Itkonen <toni.itkonen@icloud.com>
2022-10-06 20:02:50 +0300

    ThirdView

---
+++ demo1/demo1/ThirdView.swift
@@ -0,0 +1,20 @@
+//
+//  ThirdView.swift
+//  demo1
+//
+//  Created by Toni Itkonen on 6.10.2022.
+//
+
+import SwiftUI
+
+struct ThirdView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ThirdView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThirdView()
+    }
+}
